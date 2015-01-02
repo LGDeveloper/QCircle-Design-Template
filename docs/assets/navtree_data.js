8cb9aa35e286181efd4cad49036d811990ee0cbd
lyingdragon <lying_dragon@hanmail.net>
2015-01-02 10:38:56 +0900

    remove legacy folders and move the doc folder to the root folder

---
+++ docs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "com.lge.qcircle.template", "com/lge/qcircle/template/package-summary.html", [ [ "Classes", null, [ [ "QCircleBackButton", "com/lge/qcircle/template/QCircleBackButton.html", null, "" ], [ "QCircleTemplate", "com/lge/qcircle/template/QCircleTemplate.html", null, "" ], [ "QCircleTitle", "com/lge/qcircle/template/QCircleTitle.html", null, "" ], [ "TemplateTag", "com/lge/qcircle/template/TemplateTag.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "TemplateType", "com/lge/qcircle/template/TemplateType.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
